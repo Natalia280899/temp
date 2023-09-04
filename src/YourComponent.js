2aebe1dfed2abbdf2f8e195cd1813275cbd1863f
Nata G <anastasia.g@ectostar.com>
2023-09-04 17:03:53 +0400

    fix 7

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -3,42 +3,42 @@ import React, { useState, useEffect } from "react";
 
 
 export default YourComponent = () => {
-  const [input1Value, setInput1Value] = useState("");
-  const [input2Value, setInput2Value] = useState("");
+  // const [input1Value, setInput1Value] = useState("");
+  // const [input2Value, setInput2Value] = useState("");
 
-  useEffect(() => {
-    // Load saved values from localStorage when the component mounts
-    const savedInput1Value = localStorage.getItem("input1Value");
-    const savedInput2Value = localStorage.getItem("input2Value");
+  // useEffect(() => {
+  //   // Load saved values from localStorage when the component mounts
+  //   const savedInput1Value = localStorage.getItem("input1Value");
+  //   const savedInput2Value = localStorage.getItem("input2Value");
 
-    if (savedInput1Value) setInput1Value(savedInput1Value);
-    if (savedInput2Value) setInput2Value(savedInput2Value);
-  }, []);
+  //   if (savedInput1Value) setInput1Value(savedInput1Value);
+  //   if (savedInput2Value) setInput2Value(savedInput2Value);
+  // }, []);
 
-  const handleInputChange1 = (event) => {
-    const newValue = event.target.value;
-    setInput1Value(newValue);
-    localStorage.setItem("TEST:input1Value", newValue);
-  };
+  // const handleInputChange1 = (event) => {
+  //   const newValue = event.target.value;
+  //   setInput1Value(newValue);
+  //   localStorage.setItem("TEST:input1Value", newValue);
+  // };
 
-  const handleInputChange2 = (event) => {
-    const newValue = event.target.value;
-    setInput2Value(newValue);
-    localStorage.setItem("TEST:input2Value", newValue);
-  };
+  // const handleInputChange2 = (event) => {
+  //   const newValue = event.target.value;
+  //   setInput2Value(newValue);
+  //   localStorage.setItem("TEST:input2Value", newValue);
+  // };
 
   return (
     <div>
       <input
         type="text"
-        value={input1Value}
-        onChange={handleInputChange1}
+        // value={input1Value}
+        // onChange={handleInputChange1}
         placeholder="Input 1"
       />
       <input
         type="text"
-        value={input2Value}
-        onChange={handleInputChange2}
+        // value={input2Value}
+        // onChange={handleInputChange2}
         placeholder="Input 2"
       />
     </div>
